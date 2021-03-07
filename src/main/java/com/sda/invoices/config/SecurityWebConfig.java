248396d9f4346e50dd519d29cfb1c925f3edd1a8
Uzytkownik <uzytkownik@kursGIT.pl>
2021-03-07 14:03:18 +0100

    add authorization

--- src/main/java/com/sda/invoices/config/SecurityWebConfig.java
+++ src/main/java/com/sda/invoices/config/SecurityWebConfig.java
@@ -1,6 +1,10 @@
 package com.sda.invoices.config;
 
+import com.sda.invoices.utils.JwtUtil;
 import com.sda.invoices.web.filter.CorsFilter;
+import com.sda.invoices.web.filter.JwtFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -17,7 +22,16 @@ public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public FilterRegistrationBean<JwtFilter> headerValidatorFilter() {
+        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtFilter(jwtUtil));
+        registrationBean.addUrlPatterns("/products/**", "/contractor/**", "/authenticate");
+        return registrationBean;
+    }
 
+
+    private final JwtUtil jwtUtil;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,6 +39,8 @@ public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .cors().disable()
                 .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
-                .authorizeRequests().anyRequest().permitAll();
+                .authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated();
     }
 }
