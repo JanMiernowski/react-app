9915a9ec4eb50c49a423938cbd95bc59dff4dcac
rafal.oksztuski <rafal.oksztuski@pentacomp.pl>
2021-03-13 13:46:56 +0100

    Formik Fix.

--- react-app/src/LoginFormik/LoginFormik.js
+++ react-app/src/LoginFormik/LoginFormik.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useFormik} from 'formik';
+import {Formik} from 'formik';
 import {Col, Form, FormGroup, Input, Label} from "reactstrap";
 import {login} from "../Common/Api";
 import {isLogged, setAuth} from "../Common/LocalStorageService";
@@ -15,49 +15,51 @@ const LoginFormik = (props) => {
         }
         props.handleLogIn(false);
     }, []);
-    const formik = useFormik({
-        initialValues: {
-            username: '',
-            password: '',
-        },
-        onSubmit: async (values, {setSubmitting})=> {
-
-            try {
-                const response = await login(values);
-                setAuth(response);
-                props.handleLogIn(true);
-                history.push('/');
-            } catch (error) {
-                console.error(error);
-            }
-            setSubmitting(false);
-        }
-    });
     return (
         <div className={"login-form"}>
-            <Form onSubmit={event => {
-                event.preventDefault();
-                formik.onSubmit(event);
-            }}>
+          <Formik
+            onSubmit = {async (values, {setSubmitting})=> {
+                                       try {
+                                           const response = await login(values);
+                                           setSubmitting(false);
+                                           setAuth(response);
+                                           props.handleLogIn(true);
+                                           history.push('/');
+                                       } catch (error) {
+                                           setSubmitting(false);
+                                           console.error(error);
+                                       }
+                                   }
+            }
+            initialValues = {{username: '', password: ''}}
+          >
+            {({
+                handleSubmit,
+                handleChange,
+                values
+            }) => (
+            <form onSubmit={handleSubmit}>
                 <h2>Zaloguj się</h2>
                 <FormGroup row>
                     <Label sm={3} for={'username'}>Użytkownik:</Label>
                     <Col sm={9}>
-                        <Input name={'username'} value={formik.values.username} onChange={formik.handleChange}/>
+                        <Input name={'username'} value={values.username} onChange={handleChange}/>
                     </Col>
                 </FormGroup>
 
                 <FormGroup row>
                     <Label sm={3} for={'password'}>Hasło:</Label>
                     <Col sm={9}>
-                        <Input type={"password"} name={'password'} value={formik.values.password}
-                               onChange={formik.handleChange}/>
+                        <Input type={"password"} name={'password'} value={values.password}
+                               onChange={handleChange}/>
                     </Col>
                 </FormGroup>
 
                 <Input className={'btn-lg btn-dark btn-block'} type='submit' value='Dodaj'/>
 
-            </Form>
+            </form>
+            )}
+          </Formik>
         </div>
     );
 }
